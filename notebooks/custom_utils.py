f1f7ddcb0e07c975265ac81d3d91485943bf3fd3
Konrad-Grudzinski <kjgr@kth.se>
2023-10-20 18:23:07 +0200

    extracted custom utils package and adapted select stations notebook

---
+++ notebooks/custom_utils.py
@@ -0,0 +1,23 @@
+import os
+
+# constant
+year = 2023
+months = list(range(0, 7)) # January until June
+month_strings = [f"{x:02d}" for x in months]
+brand = "esso"
+cities = ["muenchen, berlin, frankfurt"]
+fuel_type = "diesel"
+rolling_window_size = 14
+
+project_base_dir = "/home/vboxuser/dic-project/"
+
+def recursive_file_retrieval(path, directory_filters = [], results = []):
+    if os.path.isfile(path):
+        results.append(path)
+    elif os.path.isdir(path):
+        for f in os.listdir(path):
+            full_path = os.path.join(path, f)
+            if os.path.isdir(full_path) and f not in directory_filters:
+                continue
+            results.extend(recursive_file_retrieval(full_path, directory_filters, []))
+    return results
